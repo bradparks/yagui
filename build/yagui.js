f25d0db4c10ffc14b97f221f9f59128b87c4b9f8
stephomi <stephaneginier@gmail.com>
2015-01-26 01:09:28 +0100

    Updates build

--- build/yagui.js
+++ build/yagui.js
@@ -747,7 +747,7 @@ define('widgets/Combobox',[
 
   
 
-  var Combobox = function ( valOrObject, callbackOrKey, options) {
+  var Combobox = function (valOrObject, callbackOrKey, options) {
     var value = this._getInitialValue(valOrObject, callbackOrKey);
     var callback = this._getCheckCallback(valOrObject, callbackOrKey);
     options = options || {};
@@ -755,13 +755,7 @@ define('widgets/Combobox',[
 
     this.domSelect = document.createElement('select');
     this.domSelect.className = 'gui-select';
-    var keys = Object.keys(options);
-    for (var i = 0; i < keys.length; ++i) {
-      var opt = document.createElement('option');
-      opt.innerHTML = options[keys[i]];
-      opt.value = keys[i];
-      this.domSelect.appendChild(opt);
-    }
+    this.addOptions(options);
 
     this.domSelect.addEventListener('change', this._onChange.bind(this));
     this.setValue(value);
@@ -771,6 +765,15 @@ define('widgets/Combobox',[
   Combobox.prototype = {
     _onChange: function (ev) {
       this.setValue(ev.target.value);
+    },
+    addOptions: function (options) {
+      var keys = Object.keys(options);
+      for (var i = 0; i < keys.length; ++i) {
+        var opt = document.createElement('option');
+        opt.innerHTML = options[keys[i]];
+        opt.value = keys[i];
+        this.domSelect.appendChild(opt);
+      }
     },
     setValue: function (val, ignoreCB) {
       this.domSelect.value = val;
